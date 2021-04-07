10a7b853b2354b6617988cbaaafc0d825b3a9746
Stevenson Grajales <57412709+StevensonGraj@users.noreply.github.com>
2021-04-07 09:04:56 -0500

    Upload .cpp file

--- OperatingSystemProject.cpp
+++ OperatingSystemProject.cpp
@@ -148,6 +148,7 @@ int main(){
                             break;
                         }
 
+                        menor = 0;
                         for(int j = 0; j < cantmarcos; j++){
                             if(auxmarcos[menor] > auxmarcos[j]){
                                 menor = j;
